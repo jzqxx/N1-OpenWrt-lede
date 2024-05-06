f5f2da84405736e82f3cfb123d0e4bd0010dc25f
jzqxx <126733798+jzqxx@users.noreply.github.com>
2024-05-06 09:08:19 +0800

    Update

--- README.md
+++ README.md
@@ -1,13 +1,7 @@
-![固件预览](https://raw.githubusercontent.com/cjlhll/N1-OpenWrt-passwall2/master/preview.png)
 固件适配情况：  
 &emsp;&emsp;本固件目前专为N1的网关服务器(旁路由)模式适配，已添加必要的iptables规则并进行了较大力度的精简。  
 >本固件默认只支持IPV4，且不能进行PPPOE拨号。
     
 插件与说明：  
-&emsp;&emsp;仅包含passwall(科学上网)，阿里ddns(远程访问)，alist，网络唤醒，docker。  
+&emsp;&emsp;仅包含passwall，ssr plus，openclash，mosdns，docker。  
 &emsp;&emsp;主题：只有默认的argon
-&emsp;&emsp;网络加速：turboacc等插件只适用于主路由模式，做网关服务器并不需要。
->本固件不会加入过多功能，需要高大全版本的朋友请用flippy大佬发布的固件。
-  
-flippy最新恩山论坛帖子：  
-&emsp;&emsp;https://www.right.com.cn/forum/thread-4076037-1-1.html
--- config/diy-part2.sh
+++ config/diy-part2.sh
@@ -4,18 +4,7 @@
 sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.5.2/g' package/base-files/files/bin/config_generate
-
-#修改密码
-sed -i 's/^root:.*:/root:$1$KVHNuqbv$4X2BPbtsXn2AApknHIn38.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
-
-# => alist
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
-git clone https://github.com/sbwml/luci-app-alist package/alist
-
-# => 阿里ddns
-git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-aliddns
+sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_generate
 
 # => dockerman
 git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
@@ -24,8 +13,18 @@ git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dock
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall_package
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 
+# => passwall2
+#git clone https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall_package
+#git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+
 # => ssr plus
-#git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+
+# => OpenClash
+git clone --depth=1 https://github.com/vernesong/OpenClash.git package/OpenClash
+
+# => mosdns
+git clone --depth=1 https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 
 # => 删除默认argon主题
 rm -rf feeds/luci/themes/luci-theme-argon
@@ -33,7 +32,3 @@ rm -rf feeds/luci/themes/luci-theme-argon
 #主题
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
-
-# 临时修复acpid,xfsprogs,aliyundrive-webdav
-#sed -i 's#flto#flto -D_LARGEFILE64_SOURCE#g' feeds/packages/utils/acpid/Makefile
-#sed -i 's#SYNC#SYNC -D_LARGEFILE64_SOURCE#g' feeds/packages/utils/xfsprogs/Makefile
